a7e73a32d7c9f684fa76dba027d8ccb123d4ae9a
soy-braised-pork <2428784410@qq.com>
2021-04-12 09:01:19 +0800

    upadte

---
+++ src/main/java/com/zlx/chat/ChannelPipelineInit.java
@@ -0,0 +1,24 @@
+package com.zlx.chat;
+
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.codec.http.websocketx.extensions.compression.WebSocketServerCompressionHandler;
+import io.netty.handler.timeout.IdleStateHandler;
+import org.springframework.web.reactive.socket.WebSocketHandler;
+
+
+public class ChannelPipelineInit extends ChannelInitializer<SocketChannel> {
+
+    @Override
+    protected void initChannel(SocketChannel socketChannel)throws Exception{
+        socketChannel.pipeline().addLast(new HttpServerCodec());
+        socketChannel.pipeline().addLast(new HttpObjectAggregator(65536));
+        socketChannel.pipeline().addLast(new WebSocketServerCompressionHandler());
+//        socketChannel.pipeline().addLast(new WebSocketHandler());
+//        socketChannel.pipeline().addLast(new IdleStateHandler(2,4,20));
+//        socketChannel.pipeline().addLast(new HeartbeatHandle());
+    }
+}
---
+++ src/main/java/com/zlx/chat/v2/Message.java
@@ -0,0 +1,45 @@
+package com.zlx.chat.v2;
+
+public class Message {
+    private String id;
+    private String content;
+    private String messageType;
+    private long timestamp;
+
+    public Message(String id,String content,long timestamp,String messageType){
+        this.id=id;
+        this.content=content;
+        this.timestamp=timestamp;
+        this.messageType=messageType;
+    }
+
+    public String getId(){
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getMessageType() {
+        return messageType;
+    }
+
+    public void setMessageType(String messageType) {
+        this.messageType = messageType;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+}
